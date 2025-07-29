bd0306e0e14e8d48c4322b744e2ee5b8b05f604d
atti <atti@gmail.com>
2025-07-29 12:13:23 +0300

    first commit loop code

---
+++ loop.py
@@ -0,0 +1,17 @@
+for i in range (0,7):
+    print(i)
+
+str_1 ="welcome"
+print(str_1)
+for i in str_1:
+    print(i)
+
+list = ["apples", "bananas", "oranges", "mangoes"]
+for i in list:
+    print(i)
+
+fruits=["guava", "apple", "banana", "oranges"]
+for item in fruits:
+    print(item)
+else:
+    print("This is the else block")
