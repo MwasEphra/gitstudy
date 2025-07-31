87e9b6dc5ddc762727f32ff10e44998380fd5427
atti <atti@gmail.com>
2025-07-31 12:58:30 +0300

    while loop code

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+counter=1
+while counter<=5:
+   print("while loop")
+   counter+=1
+
+num = 21
+counter = 1
+print("The multiplication table of ", num)
+while counter <= 10:
+    ans = num * counter
+    print(num, "x", counter, "=", ans)
+    counter += 1
+else:
+    print("loop completed successfully")
